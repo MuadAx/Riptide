96a2ef489491a4d8973b57a785f14d2ff8a90d9f
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-01 21:11:57 +0200

    Update M.py

--- M.py
+++ M.py
@@ -22,12 +22,6 @@ except ImportError:
     install('requests')
     import requests
 
-try:
-    import speedtest
-except ImportError:
-    install('speedtest-cli')
-    import speedtest
-
 TOKEN = '6150964288:AAGJOF9HCaUBYmATYoNokDi4BHaKEQAS9UA'
 bot = telebot.TeleBot(TOKEN)
 
@@ -35,13 +29,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9'
 }
-
-def test_speed():
-    st = speedtest.Speedtest()
-    st.get_best_server()
-    download_speed = st.download() / 1_000_000
-    upload_speed = st.upload() / 1_000_000
-    return download_speed, upload_speed
 
 def split_text(text, max_length=4096):
     return [text[i:i+max_length] for i in range(0, len(text), max_length)]
@@ -53,20 +40,23 @@ def start(message):
 @bot.message_handler(func=lambda message: True)
 def get_text(message):
     url = message.text
+    success = False
     for i in range(3):
         try:
             response = requests.get(url, headers=headers)
             response.raise_for_status()
+            success = True
             break
         except requests.exceptions.RequestException as e:
             print(f'Error: {e}')
+            bot.send_message(message.chat.id, "معاذ الموقع عليه ضغط")
             continue
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
-    download_speed, upload_speed = test_speed()
-    bot.send_message(message.chat.id, f'Download speed: {download_speed:.2f} Mbps\nUpload speed: {upload_speed:.2f} Mbps')
-    for part in split_text(text):
-        bot.send_message(message.chat.id, part)
+    if success:
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        for part in split_text(text):
+            bot.send_message(message.chat.id, part)
+        bot.send_message(message.chat.id, "Muad Ax done 🫡")
 
 bot.polling()
