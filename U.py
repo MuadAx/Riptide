188edef5fce91d222c3ed5f325c6eed6063be70a
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-03 07:35:52 +0200

    Update U.py

--- U.py
+++ U.py
@@ -1,22 +1,18 @@
 import subprocess
 import sys
+import telebot
+from bingpython import BingGPT
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install('EdgeGpt')
 install('bingpython')
-
-import telebot
-from EdgeGpt import EdgeGpt
-from bingpython import BingGPT
 
 API_TOKEN = '6150964288:AAGJOF9HCaUBYmATYoNokDi4BHaKEQAS9UA'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-gpt1 = EdgeGpt()
-gpt2 = BingGPT()
+gpt = BingGPT()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -24,8 +20,7 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    response1 = gpt1.generate(message.text)
-    response2 = gpt2.generate(message.text)
-    bot.reply_to(message, f"EdgeGpt: {response1}\nBingGPT: {response2}")
+    response = gpt.generate(message.text)
+    bot.reply_to(message, response)
 
 bot.polling()
