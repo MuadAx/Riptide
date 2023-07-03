ba04e2f5de9a272b0e2843681c66f6e297419a55
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-03 07:26:15 +0200

    Update U.py

--- U.py
+++ U.py
@@ -1,14 +1,20 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install('EdgeGpt')
+install('bingpython')
 
 import telebot
-from transformers import pipeline
 from EdgeGpt import EdgeGpt
 from bingpython import BingGPT
 
-API_TOKEN = '6150964288:AAGJOF9HCaUBYmATYoNokDi4BHaKEQAS9UA'
+API_TOKEN = '<your_token_here>'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 gpt1 = EdgeGpt()
 gpt2 = BingGPT()
 
@@ -18,9 +24,8 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    response1 = generator(message.text, max_length=50)[0]['generated_text']
-    response2 = gpt1.generate(message.text)
-    response3 = gpt2.generate(message.text)
-    bot.reply_to(message, f"GPT-Neo: {response1}\nEdgeGpt: {response2}\nBingGPT: {response3}")
-print("hi")
+    response1 = gpt1.generate(message.text)
+    response2 = gpt2.generate(message.text)
+    bot.reply_to(message, f"EdgeGpt: {response1}\nBingGPT: {response2}")
+
 bot.polling()
