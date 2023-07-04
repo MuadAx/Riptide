19a3c176619ef5b7a0cd097e5bde206beca5efae
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-04 18:33:46 +0200

    Update U.py

--- U.py
+++ U.py
@@ -1,5 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
 import telebot
 
 bot_token = "6150964288:AAGJOF9HCaUBYmATYoNokDi4BHaKEQAS9UA"
@@ -28,19 +28,24 @@ def get_seat_number(message):
 def get_grades(chat_id):
     url = user_data[chat_id]['url']
     seat_number = user_data[chat_id]['seat_number']
-    driver = webdriver.Firefox()
-    driver.get(url)
-    # Enter seat number
-    seat_number_field = driver.find_element_by_id("txtSeatNo")
-    seat_number_field.send_keys(seat_number)
-    # Click on view grades button
-    view_grades_button = driver.find_element_by_id("btnShow")
-    view_grades_button.click()
-    # Take screenshot
-    driver.save_screenshot("grades.png")
-    # Send screenshot to user
-    photo = open('grades.png', 'rb')
-    bot.send_photo(chat_id, photo)
-    driver.quit()
-
+    while True:
+        # Send request to get page content
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Find and fill seat number field
+        seat_number_field = soup.find('input', {'id': 'ctl00_MainContent_TxtexamineeId'})
+        seat_number_field['value'] = seat_number
+        # Find and click view grades button
+        view_grades_button = soup.find('input', {'id': 'ctl00_MainContent_btnSearch'})
+        view_grades_form = view_grades_button.find_parent('form')
+        # Send request to submit form
+        form_data = {}
+        for field in view_grades_form.find_all('input'):
+            if field.get('name'):
+                form_data[field['name']] = field.get('value')
+        response = requests.post(url, data=form_data)
+        # Send response to user
+        soup = BeautifulSoup(response.text, 'html.parser')
+        bot.send_message(chat_id, soup.get_text())
+print("hi")
 bot.polling()
