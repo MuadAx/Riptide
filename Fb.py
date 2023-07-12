b1091348bc94462c2d584ff5f035177a69f524f2
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-12 12:47:09 +0200

    Update Fb.py

--- Fb.py
+++ Fb.py
@@ -1,3 +1,18 @@
+import os
+import sys
+import subprocess
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+required_packages = ['telebot', 'cv2', 'http.client', 'requests', 'json', 'os']
+
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        install(package)
+
 import http.client
 import telebot
 import requests
