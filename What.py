ef4abe79188aa3c8937ab022a8e792801b5212c7
MuadAx <101919438+MuadAx@users.noreply.github.com>
2023-07-12 18:12:55 +0200

    Update What.py

--- What.py
+++ What.py
@@ -1,29 +1,52 @@
 import os
 import math
 import telebot
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from vidgear.gears import VideoGear
 
-TOKEN = '5566197914:AAHIoqN-wclAi8BU6vAnR_b5HQP07yPNKMw'
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 bot = telebot.TeleBot(TOKEN)
-chat_id = '1750552824'
+chat_id = 'YOUR_CHAT_ID'
 
 def send_video_info(filename):
-    clip = VideoFileClip(filename)
-    duration = clip.duration
+    stream = VideoGear(source=filename).start()
+    frame_count = 0
+    while True:
+        frame = stream.read()
+        if frame is None:
+            break
+        frame_count += 1
+    fps = stream.framerate
+    stream.stop()
+    duration = frame_count / fps
     size = os.path.getsize(filename) / (1024 * 1024)
     bot.send_message(chat_id, f'طول الفيديو: {duration} ثانية')
     bot.send_message(chat_id, f'مساحة الفيديو: {size:.2f} ميجابايت')
 
 def trim_video(filename, start_time, end_time):
-    clip = VideoFileClip(filename)
-    trimmed_clip = clip.subclip(start_time, end_time)
+    options = {"-ss": start_time, "-t": end_time - start_time}
+    stream = VideoGear(source=filename, logging=True, **options).start()
     trimmed_filename = f'trimmed_{start_time}_{end_time}_{filename}'
-    trimmed_clip.write_videofile(trimmed_filename)
+    writer = WriteGear(output_filename=trimmed_filename, logging=True)
+    while True:
+        frame = stream.read()
+        if frame is None:
+            break
+        writer.write(frame)
+    stream.stop()
+    writer.close()
     return trimmed_filename
 
 def split_video(filename, split_duration):
-    clip = VideoFileClip(filename)
-    duration = clip.duration
+    stream = VideoGear(source=filename).start()
+    frame_count = 0
+    while True:
+        frame = stream.read()
+        if frame is None:
+            break
+        frame_count += 1
+    fps = stream.framerate
+    stream.stop()
+    duration = frame_count / fps
     split_count = math.ceil(duration / split_duration)
     trimmed_filenames = []
     for i in range(split_count):
